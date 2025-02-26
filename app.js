bf887adc93699e680763c70a6221986f943bc406
angelol-git <ang.ladores.dev@gmail.com>
2025-02-26 13:56:54 -0500

    fix redirect to homepage

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/quotes', quotesRouter);
